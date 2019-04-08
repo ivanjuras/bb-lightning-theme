05a779a21fb0b53963409d2985d665aa1f213af8
Ivan Juras <ijuras@gmail.com>
2019-04-08 08:23:54 +0200

    Fix single.php

--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 
 <?php do_action( 'bblt_before_content' ); ?>
     <?php while ( have_posts() ) : the_post(); ?>
-        <?php get_template_part('templates/content', 'post'); ?>
+        <?php the_content(); ?>
     <?php endwhile; ?>
 <?php do_action( 'bblt_after_content' ); ?>
 
